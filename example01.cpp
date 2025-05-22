02c46f987f51730214d7809b750a6522f1a74b86
JeanfrancoLA <genkoz98@gmail.com>
2025-05-21 20:25:09 -0500

    First commit in my Github branch

---
+++ example01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string palabrita;
+    cout << "Ingrese una palabra: \n";
+    cin >> palabrita;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << palabrita << endl;
+    }
+
+    return 0;
+}
